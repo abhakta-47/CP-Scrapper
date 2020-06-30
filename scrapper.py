26774c012bbd54cd683cc8088e21e6ae0edc9bc3
Abhakta19 <www.arnab4@gmail.com>
2020-06-30 09:34:33 +0530

    output saved as .txt files

--- scrapper.py
+++ scrapper.py
@@ -2,34 +2,45 @@ from bs4 import BeautifulSoup
 import requests
 
 
+folder_addr = "/mnt/c/Users/ARNAB BHAKTA/Documents/cs/cp/codeforces/challanges/"
+
+
 def problem_page_scrapper(url):
     raw_data = requests.get(url).text
-
     formatted_data = BeautifulSoup(raw_data, "html5lib")
     print(formatted_data.find('div', class_="title").text+"\n")
+    # infile = open(folder_addr + formatted_data.find('table',
+    #                                                 class_='rtable').th.a.text.split('#')[1].split(' ')[0]+'/' + url.split('/', 7)[7]+"_in.txt", 'w')
+    infile = open( url.split('/', 7)[7]+"_in.txt", 'w' )
     inputs = formatted_data.find_all('div', class_="input")
     print("inputs :")
     for input_ in inputs:
         # /print(input_.pre)
         if (input_.pre.find_all('br')).__len__() != 0:
             for item in input_.pre.find_all('br'):
+                infile.write(item.previous_sibling+"\n")
                 print(item.previous_sibling)
                 # print()
         else:
+            infile.write(input_.pre.text)
             print(input_.pre.text)
         print()
-
+    infile.close()
     print("outputs :")
+    infile = open(url.split('/', 7)[7]+"_out.txt", 'w')
     outputs = formatted_data.find_all('div', class_="output")
     for output_ in outputs:
         # /print(input_.pre)
         if (output_.pre.find_all('br')).__len__() != 0:
             for item in output_.pre.find_all('br'):
+                infile.write(item.previous_sibling+"\n")
                 print(item.previous_sibling)
                 # print()
         else:
+            infile.write(output_.pre.text)
             print(output_.pre.text)
         print()
+    infile.close()
 
 
 def contest_page_scrapper(contest_url):
